f583d9bb7339950cfd9600a4cee4262b8a7d16a8
seyamasan <kst_apr_24@icloud.com>
2026-06-07 13:18:58 +0900

    fix: タイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Riverpod V3 Demo',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Riverpod V3 Demo Page'),
     );
   }
 }
