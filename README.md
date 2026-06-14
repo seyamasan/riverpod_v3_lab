9ed6ce8e454051b437d8e9194ed48eeb22d4a2f6
seyamasan <kst_apr_24@icloud.com>
2026-06-15 08:02:55 +0900

    docs: 参考記事をREADMEに追記

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # riverpod_v3_lab
 
 riverpodの3系を実験するリポジトリ。
+
+## 参考記事
+
+- [【Flutter】Riverpod 3 への移行ガイド](https://zenn.dev/harx/articles/d8c49cdec0ab1d)
--- lib/ref_extensions.dart
+++ lib/ref_extensions.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/misc.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 // Ref を拡張して、AsyncProvider の値を一度だけ取得するユーティリティを作成
-// 参考: https://zenn.dev/harx/articles/d8c49cdec0ab1d#%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B3%EF%BC%92%EF%BC%9Aawait-ref.read(provider.future)%E3%81%AE%E5%AF%BE%E5%87%A6
 extension ReadAsyncOnce on Ref {
   /// 対象 AsyncProvider の「今この瞬間の値」を一度だけ取得する。
   /// - 再計算は行わず、購読→取得→即クローズ
