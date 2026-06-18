35e8a9bd763b04dd0b29c8904794eed52620bb61
seyamasan <kst_apr_24@icloud.com>
2026-06-18 20:09:25 +0900

    test: Widgetテストのヘルパー関数

--- test/test_helper.dart
+++ test/test_helper.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -10,5 +11,19 @@ ProviderContainer createContainer({
     overrides: overrides,
     observers: observers,
     retry: (retryCount, error) => null, // リトライしない
+  );
+}
+
+// Riverpod 3.x系以降のウィジェットテスト用のヘルパー関数
+ProviderScope createScope({
+  required Widget child,
+  List<Override> overrides = const [],
+  List<ProviderObserver> observers = const [],
+}) {
+  return ProviderScope(
+    retry: (retryCount, error) => null, // <<<<< ここの設定が必要！！
+    overrides: overrides,
+    observers: observers,
+    child: child,
   );
 }
